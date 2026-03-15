53a9c67c127ecebdb50af652fcb27820f41e15ef
Théo Devaucoup <theo.devaucoup@pm.me>
2026-03-15 21:55:18 +0100

    fix base path of github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/your-reservoir-rises/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
